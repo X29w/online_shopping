93283e6f0be044c3873e4d8821f5b8fa4992fb2e
“X29w” <1937009360@qq.com>
2022-07-23 14:51:55 +0800

    完成了tabBar

---
+++ pages/Concentrated-place/Concentrated-place.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    Concentrated-place
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/Personal-Center/Personal-Center.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    Personal-Center
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
