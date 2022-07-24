77dc30f4885fb06edc5d1aef5e99e10ccd1ff478
“X29w” <1937009360@qq.com>
2022-07-24 11:16:28 +0800

    完成了home

--- main.js
+++ main.js
@@ -2,6 +2,31 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+// 导入网络请求包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+$http.beforeRequest = function(option){
+  uni.showLoading({
+    title:'sir,我马上处理好'
+  })
+}
+
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装弹框
+uni.$showMsg = function(title = 'sir，我办不到' , duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
