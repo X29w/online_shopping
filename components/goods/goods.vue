2ae9f06b6102e15633f748f39912c23c80706bd5
“X29w” <1937009360@qq.com>
2022-07-26 16:29:53 +0800

    完成了商品列表

---
+++ components/goods/goods.vue
@@ -0,0 +1,79 @@
+<template>
+  <view>
+       <view class="goods-item">
+          
+          <!-- 图片展示区 -->
+          <view class="goods-item-left">
+            <image :src="goods.goods_small_logo || defaultPic" class="goods-pic" mode=""></image>
+          </view>
+          
+          <!-- 右侧文字描述 -->
+          <view class="goods-item-right">
+            
+            <!-- 商品标题 -->
+            <view class="goods-name">{{ goods.goods_name }}</view>
+            <view class="goods-info-box">
+              <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+            </view>
+          </view>
+        </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    filters:{
+      tofixed(num){
+        return Number(num).toFixed(2)
+      }
+    },
+    props:{
+      goods:{
+        type:Object,
+        default:{}
+      }
+    },
+    name:"goods",
+    data() {
+      return {
+        defaultPic:'https://pic.616pic.com/ys_bnew_img/00/04/81/RWvymh6QCe.jpg'
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item{
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+  
+  .goods-item-left{
+    margin-right: 5px;
+    
+    
+    .goods-pic{
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+  
+  
+  .goods-item-right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 12px;
+    }
+    
+    .goods-info-box{
+      .goods-price{
+        color: #C00000;
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    good_list
+    <view class="goods-list">
+      <view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+      <goods :goods="goods"></goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,9 +12,70 @@
   export default {
     data() {
       return {
+        // 请求参数对象
+        queryObj:{
+          query:'',
+          cid:'',
+          pagenum:1,
+          pagesize:10
+        },
+        
+        goodsList:[],
+        total:0,
+        isloading:false
         
       };
-    }
+    },
+    
+    
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodList()
+    },
+    
+    
+    methods:{
+      async getGoodList(cb){
+        this.isloading = true
+        const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+        this.isloading = false
+        
+        cb && cb()
+        
+        if(res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      
+      
+      gotoDetail(item){
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id'+item.goods_id
+        })
+      }
+    },
+    
+    
+    onReachBottom() {
+      if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('sir，我知道的已经全部告诉您了')
+      if(this.isloading) return
+      this.queryObj.pagenum += 1
+      this.getGoodList()
+    },
+    
+    
+    onPullDownRefresh(){
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+      
+      this.getGoodList(()=>uni.stopPullDownRefresh())
+    },
+    
+    
+
   }
 </script>
 
